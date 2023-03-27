6fcc29763917d125aa9a0bdec8de7e82bd355b0c
bharathgoud77 <92040880+bharathgoud77@users.noreply.github.com>
2023-03-27 10:50:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
-// Get the input element and button element
-const input = document.querySelector('input[type="text"]');
-const button = document.querySelector('button');
+function calculateMinCost() {
+  var ropeLstr = document.querySelector("#rope-lengths").value;
+	var ropeLarr = ropeLstr.split(",");
+	 var n = ropeLarr.length;
 
-// Add a click event listener to the button
-button.addEventListener('click', () => {
-  // Get the array of rope lengths from the input value
-  const ropeLengths = input.value.split(',').map(Number);
-
-  // Sort the array in ascending order
-  ropeLengths.sort((a, b) => a - b);
-
-  // Initialize variables
-  let cost = 0;
-  let sum = ropeLengths[0];
-
-  // Loop through the array starting from the second element
-  for (let i = 1; i < ropeLengths.length; i++) {
-    // Add the current length to the sum
-    sum += ropeLengths[i];
-
-    // Add the sum to the cost
-    cost += sum;
-
-    // Reset the sum to the current length
-    sum = ropeLengths[i];
-  }
-
-  // Display the minimum cost in the result div
-  const result = document.getElementById('result');
-  result.textContent = `Minimum cost: ${cost}`;
-});
+	for (let i = 0; i < n; i++) {
+		ropeLarr[i] = Number(ropeLarr[i]) ; 
+	}
+	var cost =0;
+	ropeLarr.sort(function(a,b) {return a-b;	});
+	
+	while (ropeLarr.length > 1) {
+		var newRope = ropeLarr[0] + ropeLarr[1];
+		cost += newRope;
+	ropeLarr.splice(0,2);
+	ropeLarr.push(newRope);
+	}
+	ropeLarr.sort(function (a,b) { return a-b; }); 
+	
+	document.querySelector("#result").textContent = res;
+}  
